acfc377c51667269049e926523e2118daaf6372c
Jawahar112 <102861617+Jawahar112@users.noreply.github.com>
2023-04-18 18:18:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const image=document.querySelector("#img")
+var degeree=document.querySelector("#deg")
+var place=document.querySelector("#place")
+var climate=document.querySelector("#climate")
+var btn=document.querySelector("#search")
+var input=document.querySelector("#input")
+var mph=document.querySelector("#mph")
+var kph=document.querySelector("#kph")
+async function getdata(){
+const res=await fetch(`https://api.weatherapi.com/v1/current.json?key=c4ad55973ff54e0d82035204231604&q=${input.value}`,{headers: {
+      "Content-Type": "application/json"},method:"GET"})
+.then(function(response){
+return response.json();
+})
+.then(function(data){
+degeree.innerHTML=`${data.current.temp_f}°F`;
+place.innerHTML=data.location.name;
+climate.innerHTML=data.current.condition.text;
+mph.innerHTML=`${data.current.wind_mph}mph`;
+kph.innerHTML=`${data.current.wind_kph}kph`;
+if(data.current.condition.text=="Clear"){
+image.src="weathericons/clear.svg"
+}
+else if(data.current.condition.text=="Partly cloudy"){
+image.src="weathericons/cloud.svg"
+}
+else if(data.current.condition.text=="Sunny"){
+ image.src="weathericons/clear.svg"
+}
+else if(data.current.condition.text=="Moderateorheavyrainwiththunder"){
+climate.style.fontsize="5px";
+image.src="weathericons/rain.svg"
+}
+else if(data.current.condition.text == "Patchy rain possible"){
+image.src="weathericons/haze.svg"
+}
+else if(data.current.condition.text=="Mist"){
+image.src="weathericons/cloud.svg"
+}
+
+else{
+console.log("error");
+}
+})
+
+}
+btn.addEventListener("click",function(){
+getdata();
+})
